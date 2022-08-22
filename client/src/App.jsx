5b253b894607784a48a0d14692d8bb7897fe53c1
Senik <senikarakelyan@mail.ru>
2022-08-22 19:52:24 +0300

    footbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,10 @@ import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 import Home from './components/Home/Home';
 import { userCheck } from './redux/actions/userActions';
+import AboutCompany from './components/AboutCompany/AboutCompany';
+import Contacts from './components/Contacts/Contacts';
+import Footer from './components/Footer/Footer';
+import Lk from './components/Lk/Lk';
 
 function App() {
   const dispatch = useDispatch();
@@ -24,8 +28,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<AboutCompany />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/lk" element={<Lk />} />
+
         <Route component={Error} />
       </Routes>
+      <Footer />
 
     </div>
   );
