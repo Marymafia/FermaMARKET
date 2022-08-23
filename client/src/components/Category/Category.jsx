c22fcfa306367f4c2a1658f3f93d199fcb138e2c
Khakhonina Maria <khakhoninama@gmail.com>
2022-08-23 10:16:43 +0300

    categories img

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -9,12 +9,17 @@ export default function Category() {
   useEffect(() => {
     dispatch(getAllCategoryFunc());
   }, []);
-  
+
   return (
     <div className="container-fluid mx-2">
       {categories.map((el) => (
         <div className="row mt-5 mx-2">
           <div className="col-md-3">
+            <img
+              src={`http://localhost:3001/${el.img}`}
+              className="card-img-top"
+              alt="..."
+            />
             <Link key={el.id} className="nav-link active" aria-current="page" to={`/${el.id}`}>{el.name}</Link>
           </div>
         </div>
--- server/db/seeders/20220819082329-categorySeeders.js
+++ server/db/seeders/20220819082329-categorySeeders.js
@@ -11,27 +11,35 @@ module.exports = {
     */
     await queryInterface.bulkInsert('Categories', [{
       name: 'Овощи',
+      img: '/Vegetable.png',
     },
     {
       name: 'Фрукты',
+      img: '/Apple.jpeg',
     },
     {
       name: 'Молочные изделия',
+      img: '/Milckproducts.jpeg',
     },
     {
       name: 'Мясо и птица',
+      img: '/Meats.jpeg',
     },
     {
       name: 'Рыба',
+      img: '/fish2.jpeg',
     },
     {
       name: 'Мед',
+      img: '/Hony.png',
     },
     {
       name: 'Выпечка',
+      img: '/bread2.jpeg',
     },
     {
       name: 'Другое',
+      img: '/Other.jpeg',
     },
     ], {});
   },
